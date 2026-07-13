e9a41db39eab32ae1218aa8f98441a6ecafba750
Pratyaksh Kumar <pvats2118@gmail.com>
2026-07-14 00:58:58 +0530

    Day 02: Styled Compose Text using typography, colors and modifiers

--- app/src/main/java/com/example/learning_android/MainActivity.kt
+++ app/src/main/java/com/example/learning_android/MainActivity.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.learning_android.ui.theme.LearningAndroidTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learning_android.ui.theme.LearningAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,15 +54,65 @@ fun LearningScreen(name: String, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "$name Learning")
+        Text(
+            text = "$name Learning",
+            fontWeight = FontWeight.Bold,
+            fontSize = 50.sp,
+            color = Color.Red,
+            modifier = Modifier.shadow(elevation = 8.dp,shape = RoundedCornerShape(8.dp))
+        )
+
+        Spacer(modifier = Modifier.height(150.dp))
+
+        Text(
+            text = "Welcome Pratyaksh",
+            fontWeight = FontWeight.Medium,
+            fontSize = 34.sp,
+            color = Color(0xFFFFFDD0)
+        )
+
         Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Welcome Pratyaksh")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Learn • Build • Repeat")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "-------------------------------------------")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Start Learning")
+
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(color = Color.Cyan)
+                ) {
+                    append("Learn")
+                }
+                append(" • ")
+                withStyle(
+                    style = SpanStyle(color = Color.Yellow)
+                ){
+                    append ("Build")
+                }
+                append(" • ")
+                withStyle(
+                    style = SpanStyle(color = Color.Green)
+                ) {
+                    append("Repeat")
+                }
+            },
+            fontSize = 23.sp
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(horizontal = 32.dp),
+            thickness = 1.dp,
+            color = Color.Gray,
+        )
+
+        Spacer(modifier = Modifier.height(300.dp))
+
+        Text(
+            text = "Start Learning",
+            color = Color(0xFFFF7F50),
+            fontSize = 30.sp,
+            modifier = Modifier
+                .background(color = Color(0xFF1E1E24), shape = RoundedCornerShape(4.dp))
+                .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
+                .padding(16.dp)
+        )
     }
 }
 
