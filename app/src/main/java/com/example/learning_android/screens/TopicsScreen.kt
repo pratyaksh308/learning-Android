5a69db8da728f09d3ec4b51bf352a81bbad02c06
Pratyaksh Kumar <pvats2118@gmail.com>
2026-07-28 20:04:37 +0530

    feat: create clickable TopicItem cards

--- app/src/main/java/com/example/learning_android/screens/TopicsScreen.kt
+++ app/src/main/java/com/example/learning_android/screens/TopicsScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +39,17 @@ fun TopicsScreen() {
 
 @Composable
 fun TopicItem(topic: String) {
-    Text(text = topic)
+    Card(
+        modifier = Modifier.fillMaxSize(),
+        onClick = {
+
+        }
+    ){
+        Text(
+            text = topic,
+            modifier = Modifier.padding(12.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
